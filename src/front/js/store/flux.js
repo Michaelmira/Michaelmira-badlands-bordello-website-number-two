59611914697fd7fa3e31bf9e1f8b040cda3c2d71
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-29 13:29:14 +0000

    Import my super friggin cool login page i created previously success!

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,18 +2,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
 			token: undefined,
 			localStorageChecked: undefined
 		},
